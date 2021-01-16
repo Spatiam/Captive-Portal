82cb12f7f40e1de95e606b452e6925969f58cd84
hill-spatiam <hill@spatiam.com>
2021-01-16 17:02:50 -0600

    initial

---
+++ README.md
@@ -0,0 +1,49 @@
+```
+sudo -i
+```
+
+```
+curl -H 'Cache-Control: no-cache' -sSL https://raw.githubusercontent.com/tretos53/Captive-Portal/master/captiveportal.sh | sudo bash $0
+```
+
+Below sites needs to be resolvable to public IPs for CP to work
+connectivitycheck.gstatic.com
+www.gstatic.com
+www.apple.com
+captive.apple.com
+clients3.google.com
+
+Those IPs needs to be NATed to the pi so basically NAT everything from WiFi to the RPI
+
+#### Troubleshooting
+
+Install and capture traffic using `tcpdump -i wlan0 -w filename.pcap`
+
+Check nginx logs
+
+#### Other
+
+```
+
+
+# Go to /var/logs/
+cd /var/logs/
+
+# Find all gz files and extract them
+find . -name '*.gz' -execdir gunzip '{}' \;
+
+# Find MAC addresses in all files and dont show duplicates and other stuff
+grep -hoiIs '[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}' * | sort -u
+
+
+NGINX logs
+# Go to /var/logs/
+cd /var/logs/nginx/
+
+# Find all unique IP addresses that connected to the website. This will show 192...
+grep -hoiIs -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' * | sort -u
+
+# Find all unique IP addresses that connected to the website. This will show more details, like what (kind of) device connected.
+grep -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' * | sort -u```
+
+```
---
+++ captiveportal.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+	then echo "Must be root, run sudo -i before running that script."
+	exit
+fi
+
+echo "┌─────────────────────────────────────────"
+echo "|Updating repositories"
+echo "└─────────────────────────────────────────"
+apt-get update -yqq
+
+ echo "┌─────────────────────────────────────────"
+ echo "|Upgrading packages, this might take a while|"
+ echo "└─────────────────────────────────────────"
+ apt-get upgrade -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing and configuring nginx"
+echo "└─────────────────────────────────────────"
+apt-get install nginx -yqq
+cp default_nginx /etc/nginx/sites-enabled/default
+cp index.php /var/www/html/index.php
+cp index.html /var/www/html/index.html
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing dnsmasq"
+echo "└─────────────────────────────────────────"
+apt-get install dnsmasq -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring wlan0"
+echo "└─────────────────────────────────────────"
+cp dhcpcd.conf /etc/dhcpcd.conf
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring dnsmasq"
+echo "└─────────────────────────────────────────"
+cp dnsmasq.conf /etc/dnsmasq.conf
+
+echo "┌─────────────────────────────────────────"
+echo "|configuring dnsmasq to start at boot"
+echo "└─────────────────────────────────────────"
+update-rc.d dnsmasq defaults
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing hostapd"
+echo "└─────────────────────────────────────────"
+apt-get install hostapd -yqq
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring hostapd"
+echo "└─────────────────────────────────────────"
+cp hostapd.conf /etc/hostapd/hostapd.conf
+sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
+
+echo "┌─────────────────────────────────────────"
+echo "|Setting Country Code"
+echo "└─────────────────────────────────────────"
+iw reg set US
+
+echo "┌─────────────────────────────────────────"
+echo "|Configuring iptables"
+echo "└─────────────────────────────────────────"
+iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-destination 192.168.24.1:80
+iptables -t nat -A POSTROUTING -j MASQUERADE
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get -y install iptables-persistent
+
+echo "┌─────────────────────────────────────────"
+echo "|configuring hostapd to start at boot"
+echo "└─────────────────────────────────────────"
+systemctl unmask hostapd.service
+systemctl enable hostapd.service
+
+echo "┌─────────────────────────────────────────"
+echo "|Installing PHP7"
+echo "└─────────────────────────────────────────"
+apt-get install php7.3-fpm php7.3-mbstring php7.3-mysql php7.3-curl php7.3-gd php7.3-curl php7.3-zip php7.3-xml -yqq > /dev/null
+
+echo "┌─────────────────────────────────────────"
+echo "|Attempting Reboot"
+echo "└─────────────────────────────────────────"
+reboot
