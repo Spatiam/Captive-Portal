073c422e44b38810c413f1ed2b124b942c4730f4
hill-spatiam <hill@spatiam.com>
2021-02-28 13:08:07 -0600

    bash formatting

--- captiveportal.sh
+++ captiveportal.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-RED='\e[31m'
-GREEN='\e[32m'
-YELLOW='\e[33m'
-CYAN='\e[36m'
-RESET='\e[0m'
-WHITEBLACK='\e[0;30;47m'
-
 if [ "$EUID" -ne 0 ]
-	then echo "${RESET}${RED}Must be root, run sudo -i before running this script.${RESET}"
+	then echo "\e[39m\e[31mMust be root, run sudo -i before running this script.\e[39m"
 	exit
 fi
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Updating repositories${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mUpdating repositories\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get update -yqq
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Installing and configuring nginx${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mInstalling and configuring nginx\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get install nginx -yqq
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Setting up filesystem${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mSetting up filesystem\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 mkdir -p /var/www/html/images
 mkdir -p /var/www/html/passwords
 mkdir -p /var/www/html/files
@@ -45,84 +38,84 @@ cp -u /home/pi/Captive-Portal/php/submit.php /var/www/html/submit.php
 cp -u /home/pi/Captive-Portal/ion/DTN.apk /var/www/html/files/DTN.apk
 cp -u /home/pi/Captive-Portal/watchpack.py /var/www/html/watchpack.py
 mv -u /home/pi/Captive-Portal/ion/ion-open-source-4.0.2.tar.gz /home/pi/ion-open-source-4.0.2.tar.gz
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring Python${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring Python\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Installing dnsmasq${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mInstalling dnsmasq\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get install dnsmasq -yqq
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring wlan0${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring wlan0\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 cp -u /home/pi/Captive-Portal/config_files/dhcpcd.conf /etc/dhcpcd.conf
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring dnsmasq${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring dnsmasq\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 cp -u /home/pi/Captive-Portal/config_files/dnsmasq.conf /etc/dnsmasq.conf
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring dnsmasq to start at boot${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring dnsmasq to start at boot\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 update-rc.d dnsmasq defaults
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Installing hostapd${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mInstalling hostapd\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get install hostapd -yqq
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring hostapd${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring hostapd\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 cp -u /home/pi/Captive-Portal/config_files/hostapd.conf /etc/hostapd/hostapd.conf
 sed -i -- 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Setting country code${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mSetting country code\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 iw reg set US
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring iptables${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring iptables\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 iptables -t nat -A PREROUTING -s 192.168.24.0/24 -p tcp --dport 80 -j DNAT --to-destination 192.168.24.1:80
 iptables -t nat -A POSTROUTING -j MASQUERADE
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get -y install iptables-persistent
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Configuring hostapd to start at boot${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mConfiguring hostapd to start at boot\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 systemctl unmask hostapd.service
 systemctl enable hostapd.service
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Installing PHP7${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mInstalling PHP7\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get install php7.3-fpm php7.3-mbstring php7.3-mysql php7.3-curl php7.3-gd php7.3-curl php7.3-zip php7.3-xml -yqq > /dev/null
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Building watchpack service${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mBuilding watchpack service\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 sudo pip install Watchdog
 sudo pip install systemd
 apt-get install -y fswebcam
@@ -149,11 +142,11 @@ chmod +x /var/www/html/watchpack.py
 sudo systemctl daemon-reload
 sudo systemctl enable watchpack
 sudo systemctl start watchpack
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Setting up GPS${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mSetting up GPS\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 apt-get install -y gpsd gpsd-clients python-gps
 sudo systemctl stop gpsd.socket
 sudo systemctl disable gpsd.socket
@@ -176,11 +169,11 @@ EOL
 sudo killall gpsd
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
 sudo systemctl enable gpsd.socket
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Building Ion${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mBuilding Ion\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
 tar -xvzf /home/pi/ion-open-source-4.0.2.tar.gz -C /home/pi
 (cd /home/pi/ion-open-source-4.0.2 && ./configure)
 (cd /home/pi/ion-open-source-4.0.2 && make)
@@ -193,10 +186,10 @@ killm
 ionstart -I /home/pi/ion-open-source-4.0.2/dtn/mule.rc
 ss -panu
 ipcs
-echo "${GREEN}DONE"
+echo "\e[32mDONE"
 
-echo "${YELLOW}┌─────────────────────────────────────────"
-echo "|${WHITEBLACK}Reoot required${RESET}${YELLOW}"
-echo "└─────────────────────────────────────────${RESET}"
-read -n 1 -s -r -p "${CYAN}Press any key to reboot${RESET}"
+echo "\e[33m┌─────────────────────────────────────────"
+echo "|\e[0;30;47mReoot required\e[39m\e[33m"
+echo "└─────────────────────────────────────────\e[39m"
+read -n 1 -s -r -p "\e[36mPress any key to reboot\e[39m"
 reboot
