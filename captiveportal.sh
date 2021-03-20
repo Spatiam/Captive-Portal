84a82b79d5dd9e37d7c1de1e566c96bc586fb7bc
hill-spatiam <hill@spatiam.com>
2021-03-20 13:48:18 -0500

    remove pyion

--- captiveportal.sh
+++ captiveportal.sh
@@ -182,19 +182,10 @@ sudo ldconfig
 rm -r -f /home/pi/ion-open-source-4.0.2.tar.gz
 mkdir /home/pi/ion-open-source-4.0.2/dtn
 mv -u /home/pi/Captive-Portal/config_files/mule.rc /home/pi/ion-open-source-4.0.2/dtn/mule.rc
-mv -u /home/pi/Captive-Portal/ion/incoming.txt /home/pi/ion-open-source-4.0.2/dtn/incoming.txt
 killm
 ionstart -I /home/pi/ion-open-source-4.0.2/dtn/mule.rc
 ss -panu
 ipcs
-echo -e "\e[32mDONE"
-
-echo -e "\e[33m┌─────────────────────────────────────────"
-echo -e "|\e[39mBuilding pyion\e[39m\e[33m"
-echo -e "└─────────────────────────────────────────\e[39m"
-sudo mv -u /home/pi/Captive-Portal/pyion /home/pi/pyion
-export ION_HOME=/home/pi/ion-open-source-4.0.2
-(cd /home/pi/pyion && sudo -E python setup.py install)
 echo -e "\e[32mDONE"
 
 echo -e "\e[33m┌─────────────────────────────────────────"
